68bcd435c31031aa1a220ecdc60b8e40ccd4233a
Your Name <you@example.com>
2021-01-29 10:52:25 -0800

    weekend challenge

---
+++ Lab 01 - First Program/this weekend challenge.py
@@ -0,0 +1,9 @@
+print("this weekend i have drivers ed at 2:30")
+print("and I definitely have not practiced enough")
+print("sure hope i don't crash\neven though it would be kinda funny")
+print("""besides that I have red cross club at 2
+
+which is clearly a conflict
+but i don't need service hours""")
+print("\tmy sunday is completely free though which is very exciting\n\tmaybe ill make plans!")
+print(":)")
